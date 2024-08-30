06a5847198255d37b13900f647ff606020a42e53
KhanhCan2k4 <levietkhanh2k4@gmail.com>
2024-08-30 17:56:45 +0700

    fix 'React' refers to a UMD global, but the current file is a module. Consider adding an import instead.

--- App.tsx
+++ App.tsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { NavigationContainer } from "@react-navigation/native";
 import HomeScreen from "./src/views/screens/Home";
